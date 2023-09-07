eb38743d18e0446bd8ba50b364d7e87b965582d6
VineetShenvi <143125518+VineetShenvi@users.noreply.github.com>
2023-09-08 02:56:57 +0530

    Python 2

---
+++ GDSC 2/main.py
@@ -0,0 +1,20 @@
+expression= input("Enter an expression (or 'q' to quit): ")
+expression = expression.replace(',', '')
+expression = expression.replace(' ', '')
+expression = expression.replace('x', '*')
+expression = expression.replace('^', '**')
+
+valid_characters = "+-?*/()."
+try:
+    for character in expression:
+        if not (character in valid_characters or character.isnumeric()):
+            raise ValueError("Invalid characters in the expression")
+
+    result = eval(expression)
+    print(f"Result: {result}")
+
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+except Exception as e:
+    print(str(e))
+
